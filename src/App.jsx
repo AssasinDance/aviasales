02106952ea62f15fdbebe852d240f9c3882c3c7c
Danil Buntovskikh <buntovskikhds@gmail.com>
2025-06-07 14:05:16 +0300

    поправил путь до лого для продакт версии

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.scss'
 import Filter from './components/filter/filter'
 import Tickets from './components/tickets/tickets'
+import logoImage from './assets/logo.svg'
 
 export default function App() {
   return (
     <div className="main-page">
       <a className="main-page__logo-link" href="#">
-        <img className="main-page__logo" src="src\assets\logo.svg" alt="" />
+        <img className="main-page__logo" src={logoImage} alt="" />
       </a>
       <div className="main-page__content">
         <Filter />
